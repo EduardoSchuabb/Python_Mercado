6f985408555206af428320ae1aabaabeb69ee3de
Eduardo Schuabb <eduardo.schuabb@gmail.com>
2024-01-01 10:28:07 -0300

    Busca informações do indice bovespa em 3 meses, 6 meses e 1 ano.

--- script_principal.py
+++ script_principal.py
@@ -1,17 +1,45 @@
 """ ARQUIVO DE COM O SCRIPT CENTRAL PARA EXECUÇÃO DOS CÓDIGOS """
 
-from indice_bovespa import script_bovespa
 import datetime
+from dateutil.relativedelta import relativedelta
+from indice_bovespa import script_bovespa
+
 
 def main():
     """ função principal para execução do código """
+    #obter_dados_bovespa_1ano()
+
+def obter_dados_bovespa_3meses():
+    """ Método para obter dataframe com as informações do indice Bovespa no intervalo de 3 meses"""
 
     dia_atual = datetime.date.today()
-    script_bovespa.historico_bovespa("2023-01-01", dia_atual, "1d")
+    um_ano_antes = dia_atual - relativedelta(months=3)
+    print("Dia Atual: ", dia_atual)
+    print("Um ano atras: ", um_ano_antes)
+    script_bovespa.historico_bovespa(um_ano_antes, dia_atual, "1d")
+
+
+def obter_dados_bovespa_6meses():
+    """ Método para obter dataframe com as informações do indice Bovespa no intervalo de 6 meses"""
+
+    dia_atual = datetime.date.today()
+    um_ano_antes = dia_atual - relativedelta(months=6)
+    print("Dia Atual: ", dia_atual)
+    print("Um ano atras: ", um_ano_antes)
+    script_bovespa.historico_bovespa(um_ano_antes, dia_atual, "1d")
+
+def obter_dados_bovespa_1ano():
+    """ Método para obter dataframe com as informações do indice Bovespa no intervalo de 1 ano"""
+
+    dia_atual = datetime.date.today()
+    um_ano_antes = dia_atual - relativedelta(years=1)
+    print("Dia Atual: ", dia_atual)
+    print("Um ano atras: ", um_ano_antes)
+    script_bovespa.historico_bovespa(um_ano_antes, dia_atual, "1d")
+
 
 
 
 
 if __name__ == "__main__":
     main()
-
